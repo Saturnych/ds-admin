d760cea7c76b9a01b92aa18f6b122a61b99dc81a
Saturnych <saturnych@gmail.com>
2023-07-09 03:25:58 +0300

    Update stores.ts

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,4 +1,5 @@
 import { get, writable } from 'svelte/store';
+import { genAccessToken } from '$lib/utils';
 
 const userIdStore = writable('');
 export const userId = {
@@ -7,9 +8,21 @@ export const userId = {
   subscribe: (f) => userIdStore.subscribe(f),
 };
 
+const refreshTokenStore = writable('');
+export const refreshToken = {
+  get: () => get(refreshTokenStore),
+  set: (v) => refreshTokenStore.set(v),
+  subscribe: (f) => refreshTokenStore.subscribe(f),
+};
+
 const accessTokenStore = writable('');
 export const accessToken = {
   get: () => get(accessTokenStore),
   set: (v) => accessTokenStore.set(v),
   subscribe: (f) => accessTokenStore.subscribe(f),
+  generate: async () => {
+    const at = await genAccessToken(refreshToken.get());
+    if (!!at) accessTokenStore.set(at);
+    return at;
+  },
 };
