d68a4aff9bca3b1abdb009a7f1618fcec93d4c3a
Saturnych <saturnych@gmail.com>
2023-07-09 13:26:48 +0300

    Update +layout.ts

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,11 @@
 import type { LayoutLoad } from './$types';
+import { userId, accessToken } from '$lib/stores';
 import PUBLIC_ENV from '$lib/public';
 
 export const load: LayoutLoad = async ({ data }) => {
 	const session = data?.session;
-	if (PUBLIC_ENV.DEV) console.log('layout.load session:', session?.user?.email);
+	userId.set(session?.userId || '');
+	accessToken.set(session?.accessToken || '');
+	if (PUBLIC_ENV.DEV) console.log('layout.load session:', session?.user?.email, userId.get());
 	return { session };
 };
