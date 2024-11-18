9ca9c71ed7bec7f8ac5e0f59f36af652c4579c67
Denis <saturnych@gmail.com>
2024-11-18 23:13:11 +0300

    removed shit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,11 +15,6 @@
 	if (PUBLIC_ENV.DEV) console.log('page.svelte $page.data:', $page.data, uid);
 </script>
 
-<svelte:head>
-	<!-- Place this tag in your head or just before your close body tag. -->
-	<script async defer src="https://buttons.github.io/buttons.js"></script>
-</svelte:head>
-
 <div class="container mx-auto">
 	<div class="navbar bg-neutral">
 		<div class="flex-none" />
@@ -49,36 +44,6 @@
 					</div>
 				</div>
 				<h1 class="text-4xl font-bold">Dashboard!</h1>
-
-				<!-- <p class="py-6">
-					Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-					exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-				</p> -->
-				<!-- <a
-					href="https://github.com/xulioc/sveltekit-supabase-dashboard"
-					role="button"
-					class="btn btn-primary">GitHub</a
-				> -->
-
-				<!-- Place this tag where you want the button to render. -->
-				<!-- <a
-					class="github-button"
-					href="https://github.com/xulioc/sveltekit-supabase-dashboard"
-					data-color-scheme="no-preference: light; light: light; dark: dark;"
-					data-icon="octicon-star"
-					data-size="large"
-					aria-label="Star xulioc/sveltekit-supabase-dashboard on GitHub">Star</a
-				> -->
-
-				<!-- Place this tag where you want the button to render. -->
-				<!-- <a
-					class="github-button"
-					href="https://github.com/xulioc/sveltekit-supabase-dashboard/subscription"
-					data-color-scheme="no-preference: light; light: light; dark: dark;"
-					data-icon="octicon-eye"
-					data-size="large"
-					aria-label="Watch xulioc/sveltekit-supabase-dashboard on GitHub">Watch</a
-				> -->
 			</div>
 		</div>
 	</div>
